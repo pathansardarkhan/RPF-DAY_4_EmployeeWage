28e202ceb9ed6005bf8e824ba049ec1f0aab6df1
pathansardarkhan <pathan.sardarkhan6@gmail.com>
2022-05-16 12:05:38 +0530

    Sardar

---
+++ EmployeeWage/src/bridgelabparticeprogram/Employeewage.java
@@ -0,0 +1,12 @@
+package bridgelabparticeprogram;
+
+public class Employeewage {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to EmployeeWage program");
+    }
+
+
+	}
+
+
---
+++ UC-1_EmployeeAttendences/src/bridgelabparticeprogram/EmployeeAttendences.java
@@ -0,0 +1,23 @@
+package bridgelabparticeprogram;
+
+public class EmployeeAttendences {
+
+	public static void main(String[] args) {
+int IS_FULL_TIME = 1;
+    	
+    	double empCheck = Math.floor(Math.random() * 10 ) % 2;
+    	
+    	if(empCheck == IS_FULL_TIME) {
+    		System.out.println("Employee is Present");
+    	} else {
+    		System.out.println("Employee is Absent");
+    	}
+    	
+        System.out.println("Welcome to EmployeeWage program");
+    }
+
+
+
+	}
+
+
